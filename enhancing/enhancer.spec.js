4da1fb76d1bc0080230b69bddc5374ade714e57f
Natalia Beckstead <toosmiravilla@gmail.com>
2020-04-22 14:25:57 -0600

    set up project, first test

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,12 @@
-const enhancer = require('./enhancer.js');
+const { succeed, fail, repair, get } = require('./enhancer.js');
 // test away!
+describe('enchancer.js', () => {
+    it('should run test without errors', ()  => {
+       expect(true).toBe(true);
+    })
+    describe('.succeed()', function() {
+        it('return ...item', function() {
+            expect(2).toBe(2);
+        })
+    })
+});
